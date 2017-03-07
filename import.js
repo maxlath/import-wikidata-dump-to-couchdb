37ff96d86cd182d496cd0b63ffc4b883c5c6e709
maxlath <k@maxlath.eu>
2017-03-07 01:12:18 +0100

    replaced hand made reader by the line-by-line module

--- import.js
+++ import.js
@@ -1,16 +1,5 @@
 const reader = require('./lib/reader')
-const breq = require('bluereq')
-const CONFIG = require('config')
-const dbUrl = CONFIG.db.dbUrl()
-const [ file ] = Array.from(process.argv.slice(2))
+const putDocFromLine = require('./put_doc_from_line')
 
-reader(file, function (line) {
-  // remove the comma at the end of the line
-  line = line.slice(0, -1)
-  // will throw if it isnt valid JSON
-  const json = JSON.parse(line)
-  const { id } = json
-  return breq.put(`${dbUrl}/${id}`, json)
-  .then(res => console.log(res.body))
-  // .catch console.error.bind(console, 'err')
-})
+const [ file, start, end ] = process.argv.slice(2)
+reader(putDocFromLine, file, start, end)
